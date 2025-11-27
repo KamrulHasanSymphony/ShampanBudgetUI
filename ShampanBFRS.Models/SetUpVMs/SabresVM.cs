c9b277b3922637a0d0e3707ddc5391c3bdfdb0f5
DEV-1019\Monir <Monir.Hossen@symphonysoftt.com>
2025-11-27 18:47:50 +0600

    SabreController_UI

---
+++ ShampanBFRS.Models/SetUpVMs/SabresVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShampanBFRS.Models.CommonVMs;
+
+namespace ShampanBFRS.Models.SetUpVMs
+{
+    public class SabresVM : Audit
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "COA ID")]
+        public int? COAId { get; set; }
+
+        [Display(Name = "Code")]
+        public string? Code { get; set; }
+
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Remarks")]
+        public string? Remarks { get; set; }
+
+    }
+}
---
+++ ShampanBFRS.Repo/SetUpRepo/SabreRepo.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using ShampanBFRS.Models.CommonVMs;
+using ShampanBFRS.Models.KendoCommon;
+using ShampanBFRS.Models.SetUpVMs;
+using ShampanBFRS.Repo.Configuration;
+using System;
+using System.IO;
+using static ShampanBFRS.Models.CommonVMs.CommonModel;
+
+namespace ShampanBFRS.Repo.SetUpRepo
+{
+    public class SabreRepo
+    {
+        // Insert method for Sabre
+        public ResultVM Insert(SabresVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Sabre/Insert", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // Update method for Sabre
+        public ResultVM Update(SabresVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Sabre/Update", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // Delete method for Sabre
+        public ResultVM Delete(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Sabre/Delete", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // List method for Sabre
+        public ResultVM List(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Sabre/List", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // Dropdown method for Sabre
+        public ResultVM Dropdown()
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("/api/Sabre/Dropdown", authModel, JsonConvert.SerializeObject(authModel));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // GetGridData method for Sabre
+        public ResultVM GetGridData(GridOptions options)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Sabre/GetGridData", authModel, JsonConvert.SerializeObject(options,
+                    new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore
+                    }));
+
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // ReportPreview method for Sabre
+        public Stream ReportPreview(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+
+                #region Invoke API
+                var result = httpRequestHelper.PostDataReport("api/Sabre/ReportPreview", authModel, JsonConvert.SerializeObject(model));
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
