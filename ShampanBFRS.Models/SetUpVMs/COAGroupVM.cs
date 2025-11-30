bdd555eb911994c1cd0ff6cc265668990a50e1ce
DEV-1019\Monir <Monir.Hossen@symphonysoftt.com>
2025-11-30 20:01:30 +0600

    COAGroupController_Crud_UI

---
+++ ShampanBFRS.Models/SetUpVMs/COAGroupVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShampanBFRS.Models.CommonVMs;
+
+namespace ShampanBFRS.Models.SetUpVMs
+{
+    public class COAGroupVM : Audit
+    {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public int? GroupSL { get; set; }
+        public string? Category { get; set; }
+        public string? Name { get; set; }
+        public string? Remarks { get; set; }
+
+    }
+}
