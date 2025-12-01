9d91cc56b2d0b9d1bfb31de794437af380f09669
DEV-1012\Alamgir <alamgirhossain96>
2025-12-01 19:46:10 +0600

    Ceiling report

---
+++ ShampanBFRS.Repo/Reports/ReportRepo.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using ShampanBFRS.Models.CommonVMs;
+using ShampanBFRS.Repo.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ShampanBFRS.Models.CommonVMs.CommonModel;
+
+namespace ShampanBFRS.Repo.Reports
+{
+    public class ReportRepo
+    {
+
+        public ResultVM BudgetFinalReport(CommonVM options)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Ceiling/BudgetFinalReport", authModel, JsonConvert.SerializeObject(options));
+
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
---
+++ ShampanBFRSUI/Areas/Reports/ReportsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ShampanBFRSUI.Areas.Reports
+{
+    public class ReportsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Reports";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Reports_default_Area",
+                "Reports/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
