2c5560afa2c8631925ce8278cf61d7fa3cfe1a58
DEV-1019\Monir <Monir.Hossen@symphonysoftt.com>
2025-12-04 18:40:43 +0600

    Segement

--- ShampanBFRS.Models/SetUpVMs/SegmentVM.cs
+++ ShampanBFRS.Models/SetUpVMs/SegmentVM.cs
@@ -11,17 +11,17 @@ namespace ShampanBFRS.Models.SetUpVMs
     {
 
         public int Id { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public int Length { get; set; }
-        public string Remarks { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public int? Length { get; set; }
+        public string? Remarks { get; set; }
         public bool IsActive { get; set; }
         public bool IsArchive { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public string? CreatedOn { get; set; }
-        public string CreatedFrom { get; set; }
-        public string LastUpdateBy { get; set; }
+        public string? CreatedFrom { get; set; }
+        public string? LastUpdateBy { get; set; }
         public string? LastUpdateOn { get; set; }
-        public string LastUpdateFrom { get; set; }
+        public string? LastUpdateFrom { get; set; }
     }
 }
---
+++ ShampanBFRS.Repo/SetUpRepo/SegementRepo.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+using ShampanBFRS.Models.CommonVMs;
+using ShampanBFRS.Models.KendoCommon;
+using ShampanBFRS.Models.QuestionVM;
+using ShampanBFRS.Models.SetUpVMs;
+using ShampanBFRS.Repo.Configuration;
+using System;
+using System.IO;
+using static ShampanBFRS.Models.CommonVMs.CommonModel;
+
+namespace ShampanBFRS.Repo.SetUpRepo
+{
+    public class SegementRepo
+    {
+        // Insert Method
+        public ResultVM Insert(SegmentVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Segment/Insert", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Update Method
+        public ResultVM Update(SegmentVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Segment/Update", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Delete Method
+        public ResultVM Delete(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Segment/Delete", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // List Method
+        public ResultVM List(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Segment/List", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // ListAsDataTable Method
+        public ResultVM ListAsDataTable(string[] conditionalFields, string[] conditionalValues, PeramModel vm = null)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.GetData("api/Segment/ListAsDataTable", authModel);
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // Dropdown Method
+        public ResultVM Dropdown()
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.GetData("api/Segment/Dropdown", authModel);
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // GetGridData Method
+        public ResultVM GetGridData(GridOptions options)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Segment/GetGridData", authModel, JsonConvert.SerializeObject(options));
+
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // ReportPreview Method
+        public Stream ReportPreview(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
+                var result = httpRequestHelper.PostDataReport("api/Segment/ReportPreview", authModel, JsonConvert.SerializeObject(model));
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
