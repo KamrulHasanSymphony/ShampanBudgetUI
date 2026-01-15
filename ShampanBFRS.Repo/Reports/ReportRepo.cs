0870e8d4b2ec0a067741c989b2710e4f16669070
DEV-1019\Monir <Monir.Hossen@symphonysoftt.com>
2026-01-15 12:13:45 +0600

    Report_repo

--- ShampanBFRS.Repo/Reports/ReportRepo.cs
+++ ShampanBFRS.Repo/Reports/ReportRepo.cs
@@ -125,7 +125,7 @@ namespace ShampanBFRS.Repo.Reports
                 AuthModel authModel = httpRequestHelper.GetAuthentication(new CredentialModel { UserName = "erp", Password = "123456" });
 
                 #region Invoke API
-                var data = httpRequestHelper.PostData("api/ProductBudget/NonOperatingIncomeReport", authModel, JsonConvert.SerializeObject(options));
+                var data = httpRequestHelper.PostData("api/Budget/NonOperatingIncomeReport", authModel, JsonConvert.SerializeObject(options));
 
                 ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
                 #endregion
